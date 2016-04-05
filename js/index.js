077059173e009ee09db393136e7324232c9f14a8
Romesh <magneto.1990@gmail.com>
2016-04-05 17:48:50 +0530

    User validation webservice

--- js/index.js
+++ js/index.js
@@ -1959,7 +1959,7 @@ function validateValidMobileUser(){
 					j('#mainHeader').load(headerBackBtn);
              		j('#mainContainer').load(pgRef,function() {
   						document.getElementById("loginErrorMsg").innerHTML = successMessage;
-  						document.getElementById("userNameId").innerHTML=window.localStorage.getItem("UserName");
+  						document.getElementById("userName").innerHTML=window.localStorage.getItem("UserName");
 		 			   j('#loginErrorMsg').hide().fadeIn('slow').delay(4000).fadeOut('slow');
 		 			   j('#loading').hide();
 					});
