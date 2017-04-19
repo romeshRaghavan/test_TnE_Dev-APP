7778ad03aa2a795acd20379d186169c3bb77c81a
DineshSonar <dinesh.sonar93@gmail.com>
2017-04-19 15:28:48 +0530

    initial commit

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
- var j = jQuery.noConflict();
+var j = jQuery.noConflict();
 var defaultPagePath='app/pages/';
 var headerMsg = "Expenzing";
 var urlPath;
@@ -132,8 +132,8 @@ function commanLogin(){
  	var domainName = userNameValue.split('@')[1];
 	var jsonToDomainNameSend = new Object();
 	jsonToDomainNameSend["userName"] = domainName;
-        jsonToDomainNameSend["mobilePlatform"] = device.platform;
-	//jsonToDomainNameSend["mobilePlatform"] = "Android";
+	//jsonToDomainNameSend["mobilePlatform"] = device.platform;
+	jsonToDomainNameSend["mobilePlatform"] = "Android";
   	//var res=JSON.stringify(jsonToDomainNameSend);
 	var requestPath = WebServicePath;
 	j.ajax({
@@ -2168,13 +2168,14 @@ function validateValidMobileUser(){
 	         data: JSON.stringify(jsonToBeSend),
 	         success: function(data) {
 	         	
-	         	 if(data.Status == 'Success'){
-	         	 	setUserStatusInLocalStorage("Valid");
-				if(!data.MobileMapRole){
+	         if(data.Status == 'Success'){
+              window.lang.change(window.localStorage.getItem("localLanguage"));
+	          setUserStatusInLocalStorage("Valid");	 
+			/*if(!data.MobileMapRole){
 					window.localStorage.removeItem("MobileMapRole");
 				}else{
 					window.localStorage.setItem("MobileMapRole",data.MobileMapRole);
-				}
+				} */
 	           }else if(data.Status == 'NoAndroidRole'){
 	         	 	successMessage = data.Message;
 	         	 	headerBackBtn=defaultPagePath+'expenzingImagePage.html';
@@ -2999,17 +3000,16 @@ function onloadDefaultValue(){
 function viewMessages(){
 	var e = { "data" : {"address": "paytm", "body":"You have made payment of Rs.135.00 to om rest.", "date_sent":1482401219880}}
 	 saveIncomingSMSOnLocal(e);
-
 	 var e1 = { "data" : {"address": "freecharge", "body":"Recharge of BSNL mobile for Rs.54 was successful. operator refrence number is 0154324", "date_sent":1482601219880}}
 	 saveIncomingSMSOnLocal(e1);
 	 var e2 = { "data" : {"address": "uber", "body":"You paid uber Rs.134.65 with your paytm wallet. reference number for the transaction is 93123a24", "date_sent":1482701219880}}
 	 saveIncomingSMSOnLocal(e2);
 	 var e3 = { "data" : {"address": "paytm", "body":"Hi,  your order #142592342342 of Rs. 2490 for 2 items is successful. we will let you know once seller ships it.", "date_sent":1482201219880}}
-	saveIncomingSMSOnLocal(e3);
+	 saveIncomingSMSOnLocal(e3);
 	 var e4 = { "data" : {"address": "Creditcard", "body":"hi, payment of your electricity bill was successful for Rs.987.", "date_sent":1482101219880}}
-	saveIncomingSMSOnLocal(e4);
+	 saveIncomingSMSOnLocal(e4);
 
-	//console.log("viewMessages  "+filtersStr)
+	 //console.log("viewMessages  "+filtersStr)
     var headerBackBtn=defaultPagePath+'headerPageForSMSOperation.html';
     var pageRef=defaultPagePath+'fairMessageTable.html';
 	j(document).ready(function() {	
