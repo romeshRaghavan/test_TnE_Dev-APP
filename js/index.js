e658ce457004a81d896fcfedac9d8c6c3f262a5c
romeshRaghavan <magneto.1990@gmail.com>
2017-05-17 12:19:42 +0530

    Local link added for testing
    Local link added for testing

--- js/index.js
+++ js/index.js
@@ -1,9 +1,10 @@
 var j = jQuery.noConflict();
 var defaultPagePath='app/pages/';
 var headerMsg = "Expenzing";
-var urlPath;
+//var urlPath;
+var urlPath = 'http://1.255.255.169:8080/TnEV1_0AWeb/WebService/Login/'
 //var WebServicePath ='http://1.255.255.214:8085/NexstepWebService/mobileLinkResolver.service';
-var WebServicePath = 'http://live.nexstepapps.com:8284/NexstepWebService/mobileLinkResolver.service';
+//var WebServicePath = 'http://live.nexstepapps.com:8284/NexstepWebService/mobileLinkResolver.service';
 //var WebServicePath ='http://1.255.255.36:9898/NexstepWebService/mobileLinkResolver.service';
 var clickedFlagCar = false;
 var clickedFlagTicket = false;
@@ -49,7 +50,7 @@ function login()
     var jsonToBeSend=new Object();
     jsonToBeSend["user"] = userName.value;
     jsonToBeSend["pass"] = password.value;
-	//setUrlPathLocalStorage(urlPath);
+	setUrlPathLocalStorage(urlPath);
 	urlPath=window.localStorage.getItem("urlPath");
 	j('#loading').show();
     j.ajax({
