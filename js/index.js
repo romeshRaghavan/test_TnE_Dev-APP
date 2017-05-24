9f9448be79892fbbbb9150830880e9447d805da2
romeshRaghavan <magneto.1990@gmail.com>
2017-05-24 12:20:48 +0530

    url change for sbi
    url change for sbi

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ var j = jQuery.noConflict();
 var defaultPagePath='app/pages/';
 var headerMsg = "Expenzing";
 //var urlPath;
-var urlPath = 'http://1.255.255.169:8080/TnEV1_0AWeb/WebService/Login/'
+var urlPath = 'http://1.255.255.36:13130/TnEV1_0AWeb/WebService/Login/'
 //var WebServicePath ='http://1.255.255.214:8085/NexstepWebService/mobileLinkResolver.service';
 //var WebServicePath = 'http://live.nexstepapps.com:8284/NexstepWebService/mobileLinkResolver.service';
 //var WebServicePath ='http://1.255.255.36:9898/NexstepWebService/mobileLinkResolver.service';
